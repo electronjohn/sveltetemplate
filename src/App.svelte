91fbbb8869fe7774fd2d64fd0d92aac4b0cd5987
electronjohn <johnporterfield@gmail.com>
2022-09-26 22:49:55 -0500

    chapter 2 interim (fixed author bug)

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,29 @@
 <script>
-	import {fade} from 'svelte/transition';
-	let books = ["Learning Svelte", "The Zen of Cooking Tea"];
-	let newBook = "";
+	let todos = ["learn Svelte", "Create Something"]; // an empty list
+	let newTodo = "";
 
-	function addBook(evt) {
-		if (evt.key == "Enter" && newBook != "") {
-			books = [...books, newBook];
-			newBook = "";
+	function handleKeyup(evt){
+		if (evt.key == 'Enter'){
+			todos = [...todos, newTodo];
+			newTodo = "";
+		}
+		else{
+			newTodo = evt.target.value;
 		}
 	}
 </script>
 
-<main>
-	<h1>My Little Book App</h1>
-	<h2>Add Book</h2>
-	<input type="text" bind:value={newBook} on:keydown={addBook} />
-	<h2>My Books</h2>
-	{#each books as book}
-		<li transition:fade>{book}</li>
-	{/each}
-</main>
+<label>
+	New To-Do
+	<input type="text" value={newTodo} on:keyup={handleKeyup} />
+</label>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: white;
-		font-size: 3em;
-		font-weight: 100;
-		background: teal;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
-	main {
-		background-image: linear-gradient(teal, white);
-	}
-
-	input {
-		padding: 5px 10px;
-	}
-	li {
-		list-style:none;
-	}
-</style>
+{#if todos.length == 0}
+	<p>What do you want to work on?</p>
+{:else}
+	<ul>
+		{#each todos as todo}
+			<li>{todo}</li>
+		{/each}
+	</ul>
+{/if}
