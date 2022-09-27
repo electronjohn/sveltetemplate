44410be7fb976a0f28cc9c694255c3e550667d33
electronjohn <johnporterfield@gmail.com>
2022-09-26 22:28:58 -0500

    Chapter 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
-	export let name;
+	import {fade} from 'svelte/transition';
+	let books = ["Learning Svelte", "The Zen of Cooking Tea"];
+	let newBook = "";
+
+	function addBook(evt) {
+		if (evt.key == "Enter" && newBook != "") {
+			books = [...books, newBook];
+			newBook = "";
+		}
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>My Little Book App</h1>
+	<h2>Add Book</h2>
+	<input type="text" bind:value={newBook} on:keydown={addBook} />
+	<h2>My Books</h2>
+	{#each books as book}
+		<li transition:fade>{book}</li>
+	{/each}
 </main>
 
 <style>
@@ -16,15 +30,29 @@
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
+		color: white;
+		font-size: 3em;
 		font-weight: 100;
+		background: teal;
 	}
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+	}
+
+	main {
+		background-image: linear-gradient(teal, white);
+	}
+
+	input {
+		padding: 5px 10px;
+	}
+	li {
+		list-style:none;
+	}
+	ul {
+		padding: 5px 0;
 	}
 </style>
